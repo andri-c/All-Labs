1ef7ae8736c72b10c38e541eeb532c9705dba61d
Andrew Chang <andrewchang2906@gmail.com>
2025-08-31 21:29:17 -0700

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Hello! My first java scripting class and lesson! This is my first time using the BlueJ environment to do coding.
+ * I have only experimented in the jupyter notebook prior to this class.
+ * @author Andrew Chang, andrewchang2906@gmail.com/achang013@student.sdccd.edu
+ * @since 8/31/2025
+ * @version v1.0
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Andrew Chang");
+    }
+}
