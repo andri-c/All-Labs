6afd5ac6da3231044ab8779e8103bd2016f6e566
andri-c <143442846+andri-c@users.noreply.github.com>
2025-11-09 21:23:01 -0800

    lab 7 part 2

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 /**
  * This ArrayMethods class helps give the foundation about arrays and how we can manipulate them with the aspects stated below.
  * This counts the number of elements in myArray, the sum, the average, the maximum, and the index of maximum.
- * 
+ * Part 2 also dives deeper into manipulating arrays such as copying them and returning them.
  * 
  * @author Andrew Chang, andrewchang2906@gmail.com
  * @version v1.0
@@ -81,7 +81,7 @@ public class ArrayMethods {
     }
 
     /**
-     * This prints the array by taking all the information above and printing it out.
+     * This prints the array by taking all the information above and printing it out. (Thanks for giving this)
      * 
      * @param inArray int array to print
      */
@@ -95,5 +95,75 @@ public class ArrayMethods {
             System.out.print(inArray[i]);
         }
         System.out.println("}");
+    }
+    
+     /**
+     * Finds the last (rightmost) index of a key in myArray.
+     * 
+     * @param key value to search for
+     * @return index of last occurrence, or -1 if not found
+     */
+    public int findLast(int key) {
+        for (int i = myArray.length - 1; i >= 0; i--) {
+            if (myArray[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Finds all indices where a given key appears in myArray.
+     * 
+     * @param key value to search for
+     * @return array of indices where key occurs
+     */
+    public int[] findAll(int key) {
+        int count = 0;
+        for (int num : myArray) {
+            if (num == key)
+                count++;
+        }
+
+        int[] indices = new int[count];
+        int pos = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == key) {
+                indices[pos++] = i;
+            }
+        }
+        return indices;
+    }
+
+    /**
+     * Returns a "getArray" back to myArray.
+     * 
+     * @return the original myArray
+     */
+    public int[] getArray() {
+        return myArray;
+    }
+
+    /**
+     * Returns a copy of myArray using Arrays.copyOf().
+     * 
+     * @return a copied version of myArray
+     */
+    public int[] copyArray() {
+        return Arrays.copyOf(myArray, myArray.length);
+    }
+
+    /**
+     * Reverses the elements of the given array (in place).
+     * (Modifies the passed array directly.)
+     * 
+     * @param inArray array to reverse
+     */
+    public void reverseArray(int[] inArray) {
+        for (int i = 0; i < inArray.length / 2; i++) {
+            int temp = inArray[i];
+            inArray[i] = inArray[inArray.length - 1 - i];
+            inArray[inArray.length - 1 - i] = temp;
+        }
     }
 }
